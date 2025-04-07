c3ba0093cc9797007052e76b7293338bccaec65f
Ukachi Charles <ukcharlies@gmail.com>
2025-04-07 16:46:29 +0100

    Initial phase digital frontend

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#7EA3CC',
+        secondary: '#3D4576',
+        accent: '#ACC18A',
+        secondaryDarker: '#2C3454',
+      },
+    },
+  },
+  plugins: [],
+}
